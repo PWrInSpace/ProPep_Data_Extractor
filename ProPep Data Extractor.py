f4990102de85b89b34d56ed34551557e376c5e50
Sebastian Król <seba.krol07@gmail.com>
2025-11-29 12:22:13 +0100

    Fixed the k and T parameters being taken from nozzle instead of chamber
    Refactor ProPep Data Extractor to handle chamber results and improve data processing.

--- ProPep Data Extractor.py
+++ ProPep Data Extractor.py
@@ -21,6 +21,8 @@ Run = 1
 OF = []
 M = []
 dataFlag = False
+dataFlag2 = False
+CHAMBER_FLAG = False
 Isp = []
 T = []
 k = []
@@ -35,6 +37,8 @@ for line in lines:
             if char == "o":
                 break
         Run = int(Run)
+        if Run > 53:
+            pass
         ox_percentage = ox_min + step*(Run-1)
         OF.append(ox_percentage / (100-ox_percentage))
         M.append("")
@@ -60,12 +64,58 @@ for line in lines:
 
         M[Run-1] = float(M[Run-1])
         print("Molecular weight: ", M[Run-1], " g/mol")
+        # ------------------------------------
+
+    if ("CHAMBER RESULTS FOLLOW" in line or CHAMBER_FLAG):
+        if CHAMBER_FLAG:
+            CHAMBER_FLAG = False
+            dataFlag2 = True
+            continue
+        else:
+            CHAMBER_FLAG = True
+            continue
+
+    if dataFlag2 == True:
+        i = 0 # 0 - T, 6 - k
+        i2 = 0
+        nextFlag = False
+        for char in line:
+            if char.isnumeric() or char == "," or char == "." or char == "-":
+                nextFlag = True
+                i2 = 0
+                if char == ",":
+                    char = "."
+                if i == 0: # T
+                    T[Run-1] = T[Run-1]+char
+                if i == 6: # k
+                    k[Run-1] = k[Run-1]+char
+            else:
+                if nextFlag == True:
+                    i = i+1
+                    nextFlag = False
+            if i>6:
+                k[Run-1] = float(k[Run-1])
+                T[Run-1] = float(T[Run-1])
+                print("Isentropic exponent: ", k[Run-1])
+                print("Total temperature: ", T[Run-1], " K")
+                break
+            i2 = i2+1
+            if i2 > 6:
+                k[Run-1] = -1
+                T[Run-1] = -1
+                print("Isentropic exponent: ", k[Run-1])
+                print("Total temperature: ", T[Run-1], " K")
+                i2 = 0
+                break
+        dataFlag2 = False
+
+        # ---------------------------------------
     if ("IMPULSE" in line):
         dataFlag = True
         continue
 
     if dataFlag == True:
-        i = 0 # 0 - ISP, 1 - k, 2 - T*, 4 - c*
+        i = 0 # 0 - ISP, 4 - c*
         i2 = 0
         nextFlag = False
         for char in line:
@@ -76,10 +126,6 @@ for line in lines:
                     char = "."
                 if i == 0: # Isp
                     Isp[Run-1] = Isp[Run-1]+char
-                if i == 1: # k
-                    k[Run-1] = k[Run-1]+char
-                if i == 2: # T*
-                    T[Run-1] = T[Run-1]+char
                 if i == 4: # c*
                     cstar[Run-1] = cstar[Run-1]+char
             else:
@@ -88,23 +134,15 @@ for line in lines:
                     nextFlag = False
             if i>=5:
                 Isp[Run-1] = float(Isp[Run-1])
-                k[Run-1] = float(k[Run-1])
-                T[Run-1] = float(T[Run-1])
                 cstar[Run-1] = float(cstar[Run-1])*0.3048
                 print("Specific impulse: ", Isp[Run-1], " s")
-                print("Isentropic exponent: ", k[Run-1])
-                print("Total temperature: ", T[Run-1], " K")
                 print("Characteristic velocity: ", cstar[Run-1], " m/s")
                 break
             i2 = i2+1
             if i2 > 5:
                 Isp[Run-1] = -1
-                k[Run-1] = -1
-                T[Run-1] = -1
                 cstar[Run-1] = -1
                 print("Specific impulse: ", Isp[Run-1], " s")
-                print("Isentropic exponent: ", k[Run-1])
-                print("Total temperature: ", T[Run-1], " K")
                 print("Characteristic velocity: ", cstar[Run-1], " m/s")
                 i2 = 0
                 break
